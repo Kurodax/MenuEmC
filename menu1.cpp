d43e36528a03f8300597ee2b2dfb731563cf4a7e
Lucas Kuroda <lucas.kuroda@ikatec.com.br>
2020-11-12 10:47:23 -0300

    melhoria 2

--- menu1.cpp
+++ menu1.cpp
@@ -16,12 +16,12 @@ int main()
         printf("3 - opcao 3\n");
         printf("0 - sair\n");
         scanf("%d", &op);
+        system("clear");
 
         if (op == 1)
         {
             do
             {
-                system("clear");
                 printf("### Calcular a área de um retângulo ####\n\n\n");
                 printf("Favor entrar com as informações solicitada abaixo: \n");
 
@@ -40,18 +40,18 @@ int main()
                 {
                     printf("Digite 0 para voltar ao menu...\n");
                     printf("Digite 1 para calcular novamente\n");
+                    scanf("clear");
                     scanf("%d", &deveRetornar);
 
                     if (deveRetornar != 0 && deveRetornar != 1)
                     {
-                        printf("Valor invalido");
+                        printf("Valor invalido\n\n");
                     }
                 } while (deveRetornar != 0 && deveRetornar != 1) ;
             } while (deveRetornar != 0);
         }
         else if (op == 2)
         {
-            system("clear");
             printf("### DATA NASCIMENTO ####\n\n\n\n");
 
             printf("digite 0 para voltar ao menu...");
@@ -59,7 +59,6 @@ int main()
         }
         else if (op == 3)
         {
-            system("clear");
             printf("### ESTÁ NO OPÇÃO 3 ####\n\n\n\n");
 
             printf("digite 0 para voltar ao menu...");
@@ -67,14 +66,12 @@ int main()
         }
         else if (op == 0)
         {
-            system("clear");
             printf("OBRIGADO POR TER USADO NOSSO SISTEMA.");
             scanf("%u", &op);
             exit(0);
         }
         else
         {
-            system("clear");
             printf("Voce digitou uma opcao invalida!!!!!!\n\n\n");
             printf("digite algo para voltar ao menu...");
             scanf("%u", &op);
