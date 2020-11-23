7d31e453a422f71ec36122b09b1476436dda77dc
Lucas Kuroda <lucas.kuroda@ikatec.com.br>
2020-11-23 09:06:15 -0300

    menuc

--- 32main.cpp
+++ 32main.cpp
@@ -1,36 +1,19 @@
-// #include <iostream> 
-// #include <cmath>
-
-// using namespace std;
-
-// #define PI 3.14159
-
-// int main(){
-
-//    int raio;
-//    float area;
-
-//    printf("Entre com o valor do raio: ");
-//    scanf("%i", raio);
-//    area = PI * pow(raio, 2);
-//    printf("Área é ", area);
-// }
 #include <stdio.h>
+#include <cmath>
 #include <math.h>
-
 
 #define PI 3.14159
 
-int main(void)
+int main()
 {
-	float raio, pi=3.14, area;
-	
-      printf("Entre com o valor do raio: "); 
-      scanf("%f", &raio);
-   
-      area = pi * raio * raio;
-   
-      printf("AREA = %.2f m2 \n", area);
 
-	return 0;
+      float vRaio, vArea;
+
+      printf("\nInforme o raio da círculo: ");
+      scanf("%f", &vRaio);
+
+      vArea = PI * (vRaio * vRaio);
+
+      printf("\nA Área da circunferência é: %2f", (vArea));
+      printf("\n\n");
 }
--- menu1.cpp
+++ menu1.cpp
@@ -2,84 +2,95 @@
 #include <stdlib.h>
 #include <termios.h>
 
+void calcularRetangulo()
+{
+    // system("clear");
+    printf("### Calcular a área de um retângulo ###\n\n\n");
+    printf("Favor entrar com as informações solicitada abaixo: \n");
+
+    float largura, comprimento, area;
+    do
+    {
+
+        printf("LARGURA = ");
+        scanf("%f", &largura);
+
+        printf("COMPRIMENTO = ");
+        scanf("%f", &comprimento);
+        if (largura <= 0 || comprimento <= 0)
+            printf("\n # IMPOSSIVEL CALCULAR #\nUtilize números positivos nas informações\n");
+
+    } while (largura <= 0 || comprimento <= 0);
+
+    area = largura * comprimento;
+
+    printf("AREA = %f m2 \n\n\n", area);
+    return;
+}
+
+void calcularCirculo()
+{
+    // system("clear");
+    printf("### Calcular a área de um Círculo ###\n\n\n");
+    printf("Favor entrar com a informação solicitada abaixo: \n");
+
+    #define PI 3.14159
+    float vRaio, vArea;
+
+    printf("\nInforme o raio da círculo: ");
+    scanf("%f", &vRaio);
+
+    vArea = PI * (vRaio * vRaio);
+
+    printf("\nA Área da circunferência é: %2f\n\n\n", (vArea));
+};
+
+void deveRepetir(int &deveRetornar, int v)
+{
+    do
+    {
+        printf("Digite 0 para voltar ao menu principal\n");
+        printf("Digite para utilizar novamente a calculadora\n", &v);
+        scanf("%d", &deveRetornar, &v);
+
+        if (deveRetornar != 0 && deveRetornar != v)
+        {
+            // system("clear");
+            printf("### Valor Invalido ###\n\n");
+        }
+    } while (deveRetornar != 0 && deveRetornar != v);
+}
+
 int main()
 {
 
     int op = 0, deveRetornar;
+    bool repete = false;
 
     do
     {
-        system("clear");
-        printf("   ### MENU ####\n");
-        printf("1 - CALCULAR - Retângulo\n");
-        printf("2 - CALCULAR - Círculo\n");
-        printf("0 - SAIR\n");
-        scanf("%d", &op);
-        system("clear");
+        if (!repete)
+        {
+            // system("clear");
+            printf("   ### MENU ####\n");
+            printf("1 - CALCULAR - Retângulo\n");
+            printf("2 - CALCULAR - Círculo\n");
+            printf("0 - SAIR\n");
+            scanf("%d", &op);
+            // system("clear");
+        }
 
         if (op == 1)
         {
-            do
-            {
-                system("clear");
-                printf("### Calcular a área de um retângulo ####\n\n\n");
-                printf("Favor entrar com as informações solicitada abaixo: \n");
-
-                float largura, comprimento, area;
-
-                printf("LARGURA = ");
-                scanf("%f", &largura);
-
-                printf("COMPRIMENTO = ");
-                scanf("%f", &comprimento);
-
-                area = largura * comprimento;
-
-                printf("AREA = %f m2 \n\n\n", area);
-                do
-                {
-                    printf("Digite 0 para voltar ao menu principal...\n");
-                    printf("Digite 1 para utilizar novamente a calculadora\n");
-                    scanf("%d", &deveRetornar);
-
-                    if (deveRetornar != 0 && deveRetornar != 1)
-                    {
-                        system("clear");
-                        printf("### Valor Invalido ###\n\n");
-                    }
-                } while (deveRetornar != 0 && deveRetornar != 1);
-            } while (deveRetornar != 0);
+            calcularRetangulo();
+            deveRepetir(deveRetornar, 1);
+            repete = deveRetornar;
         }
         else if (op == 2)
         {
-            do
-            {
-                system("clear");
-                printf("### Calcular a área de um Círculo ####\n\n\n");
-                printf("Favor entrar com a informação solicitada abaixo: \n");
-
-                #define PI 3.14159
-                float vRaio, vArea;
-
-                printf("\nInforme o raio da círculo: ");
-                scanf("%f", &vRaio);
-
-                vArea = PI * (vRaio * vRaio);
-
-                printf("\nA Área da circunferência é: %2f\n\n\n", (vArea));
-                do
-                {
-                    printf("Digite 0 para voltar ao menu principal...\n");
-                    printf("Digite 2 para utilizar novamente a calculadora\n");
-                    scanf("%d", &deveRetornar);
-
-                    if (deveRetornar != 0 && deveRetornar != 2)
-                    {
-                        system("clear");
-                        printf("### Valor Invalido ###\n\n");
-                    }
-                } while (deveRetornar != 0 && deveRetornar != 2);
-            } while (deveRetornar != 0);
+            calcularCirculo();
+            deveRepetir(deveRetornar, 2);
+            repete = deveRetornar;
         }
         else if (op == 0)
         {
@@ -91,14 +102,14 @@ int main()
         {
             do
             {
-                system("clear");
+                // system("clear");
                 printf("OPS, Você digitou uma opcão inválida!\n\n\n");
                 printf("Digite 0 para voltar ao menu principal...\n");
                 scanf("%u", &op);
-            } while (deveRetornar != 0 || deveRetornar == 0);
+            } while (op != 0);
         }
 
-    } while (op != 3);
+    } while (true);
 
     return 0;
 }
